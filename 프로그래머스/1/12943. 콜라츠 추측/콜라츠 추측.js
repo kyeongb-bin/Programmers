1395cc9d29b0b50d8e684b4121408fb16ee78125
김경빈 <154494689+kyeongb-bin@users.noreply.github.com>
2024-03-28 23:33:57 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,17 @@
+function solution(num) {
+    var answer = 0;
+    
+    while(num != 1){
+        answer++;
+        if(answer == 500){
+            answer = -1;
+            break;
+        }
+        if((num % 2) == 0){
+            num = num / 2;
+        } else{
+            num = num * 3 + 1;
+        }
+    }
+    return answer
+}
