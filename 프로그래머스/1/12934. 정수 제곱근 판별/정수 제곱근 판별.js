29e79c771c489e03b6557eb4e4fc7cf9cd85b942
김경빈 <154494689+kyeongb-bin@users.noreply.github.com>
2024-03-28 18:12:09 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.05 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,6 +1,9 @@
 function solution(n) {
+    //Math.sqrt는 제곱근을 알아냄
     const sqrt = Math.sqrt(n);
+    //Number.isInteger는 정수인지 확인
     if (Number.isInteger(sqrt)) {
+        //정수면 Math.pow로 1을 더하고 제곱 함
         return Math.pow(sqrt + 1, 2);
     }
     return -1;
