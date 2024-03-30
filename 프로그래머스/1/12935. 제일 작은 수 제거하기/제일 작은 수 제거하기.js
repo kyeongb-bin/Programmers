8f0bc616caa8bdba4c5659d3d691712df85b8634
김경빈 <154494689+kyeongb-bin@users.noreply.github.com>
2024-03-30 16:17:14 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,13 +1,16 @@
 function solution(arr) {
+    if (arr.length == 1) {
+        return [-1];
+    }
+    
     let min = 0;
-    for(let i = 0; i < arr.length; i++){
-        if(arr[min] > arr[i+1]){
-            min = i+1;
+    
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[min] > arr[i + 1]) {
+            min = i + 1;
         }
     }
-    if(arr.length == 1){
-        arr.push(-1);
-    }
+    
     arr.splice(min, 1);
     return arr;
 }
